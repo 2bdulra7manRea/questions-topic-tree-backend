128d3ad8dde3dae697c4fdf8aab6f19de61dfa95
abdelrahmanMosa <abdomosa987@gmail.com>
2022-03-27 15:48:39 +0200

    readme

--- db/config.js
+++ db/config.js
@@ -4,16 +4,6 @@ import mongoose  from "mongoose"
 import migratesData from "./mirgation/migration.js"
 
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://petro:ausbildung@questionscluster.pawtr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-
 let url="mongodb+srv://petro:pmp@questionscluster.pawtr.mongodb.net/pencileDB?retryWrites=true&w=majority"
 
 mongoose.connect(url,(error)=>{
