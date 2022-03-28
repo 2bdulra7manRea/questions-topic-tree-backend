7f27a2cd91582a525e0ce26e9023d835be908315
Abdelrahman Mosa <79328931+2bdulra7manRea@users.noreply.github.com>
2022-03-28 10:32:27 +0200

    error handling

--- controller/questions.js
+++ controller/questions.js
@@ -1,14 +1,15 @@
 import * as questionsService from "../services/questions.js"
 const search=async (req,res)=>{
-    const {query}=req;    
-    if(!query.q){
-    res.json([])
-    return;
+    try {
+        const {query}=req;    
+        if(!query.q){
+        res.json([])
+        }
+        let results=await questionsService.search(query)
+        res.json(results)
+    } catch (error) {
+        res.json({message:error})
     }
-    let results=await questionsService.search(query)
-    res.json(results)
-    }
+}
     
-
-
 export {search}
